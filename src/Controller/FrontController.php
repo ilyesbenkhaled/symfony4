8038ce0296c9684decc5c108a059c9c586b7c2ee
Ilyes Khaled <ilyeskhaled@gmail.com>
2020-02-05 17:58:01 +0100

    projet symfony 4

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+
+class FrontController extends AbstractController
+{
+    
+    /**
+     * @Route("/", name="index")
+     */
+    
+    public function index()
+    {
+        
+       return $this->render('front/index.html.twig');
+        
+    }
+     /**
+     * @Route("/liste", name="liste")
+     */
+    
+    public function Liste()
+    {
+       $listes = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+       // print_r($listes);
+        return $this->render('menu.html.twig', ['listes' => $listes ]);
+        
+    }
+    
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function deleteAction($id)
+    {
+        
+        $data = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy(array('id' => $id));
+        
+
+    /*  $currentUserId = 1;
+     print_r($currentUserId);
+      if ($currentUserId == $id)
+      {
+        $session = $this->get('session');
+        $session = new Session();
+        $session->invalidate();*/
+    $em = $this->getDoctrine()->getManager();
+    $em->remove($data);
+    $em->flush();
+     
+    return $this->redirectToRoute('liste');
+       
+     }
+    
+}
+
+
---
+++ src/Entity/Avatar.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AvatarRepository")
+ */
+class Avatar
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $path;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $info;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(?string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getInfo(): ?string
+    {
+        return $this->info;
+    }
+
+    public function setInfo(string $info): self
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+}
