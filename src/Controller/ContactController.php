55e482382792ecf3e22e10e70027b8a711a68753
Ilyes Khaled <ilyeskhaled@gmail.com>
2020-04-08 18:56:39 +0100

    08/04/2020

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,41 +8,43 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
 use Symfony\Component\Form\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use MercurySeries\FlashyBundle\FlashyNotifier;
 
 class ContactController extends AbstractController {
+
 
     /**
     * @Route( "/contact", name = "contact" )
     */
 
-    public function contact( Request $request, \Swift_Mailer $mailer ) {
+    public function contact( Request $request, \Swift_Mailer $mailer, FlashyNotifier $flashy) {
 
         $form = $this->createForm( ContactType::class );
         $form->handleRequest($request);
 
         if ( $form->isSubmitted() && $form->isValid() ) {
 
-            $this->addFlash('success', 'Message is sent successfully'); 
-            
+            $flashy->success('Welcome Aboard', 'http://your-awesome-link.com');
+
+           $this->addFlash('success', 'Message is sent successfully'); 
             $contactEmail = $form->getData();
-           // print_r($contactEmail);
+          //  print_r($flashy);
           
             $message = ( new \Swift_Message( 'Hello Email im a new email' ) )
              
-->setFrom($contactEmail['email'])
-->setTo('nutritionaecor@gmail.com')
+            ->setFrom($contactEmail['email'])
+            ->setTo('nutritionaecor@gmail.com')
             ->setReplyTo($contactEmail['email'])
             ->setBody($this->renderView(
                 'emails/registration.html.twig', 
                 ['cont' => $contactEmail])
         , 'text/html');
             $mailer->send( $message );
-            
+
             return $this->redirectToRoute('contact');           
         }
 
         
-
         return $this->render( 'contact/contact.html.twig', [
             'form' => $form->createView()
             ] );
