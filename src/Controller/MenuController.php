c38cdfea30bd69164be065f3fa2d00df4519d91b
Ilyes Khaled <ilyeskhaled@gmail.com>
2020-09-30 17:39:39 +0100

    30/09

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -6,19 +6,20 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MenuController extends AbstractController {
+class MenuController extends AbstractController
+{
 
     /**
-    * @Route( "/menu", name = "menu" )
-    * @author dev khaled
-    */
+     * @Route( "/menu", name = "menu_liste" )
+     * @author dev khaled
+     */
 
-    public function menu( $max = 3 ) {
+    public function menu($max = 3)
+    {
         $product = $this->getDoctrine()
-        ->getRepository( Product::class )
-        ->findAll();
+            ->getRepository(Product::class)
+            ->findAll();
 
-        return $this->render( 'menu.html.twig', ['articles' => $product] );
+        return $this->render('menu.html.twig', ['articles' => $product]);
     }
-
 }
